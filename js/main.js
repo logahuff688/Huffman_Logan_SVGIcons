578918453d61e6389e4b9fa69f30ac9625010971
logahuff688 <l_huffman159588@fanshaweonline.ca>
2023-01-10 13:08:12 -0500

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 document.addEventListener("click", (event) => {
   const element = event.target;
 
+  const elementAlt = element.getAttribute("alt");
   const elementId = element.getAttribute("id");
 
   event.target.classList.toggle("selected");
 
-  console.log(`You clicked on the element with ID: ${elementId}`);
+  console.log(
+    `You clicked on the element with ID: ${elementId},\n that is a ${elementAlt}.`
+  );
   console.log(
     `the class, "${event.target.classList}" has been added to ${elementId}`
   );
