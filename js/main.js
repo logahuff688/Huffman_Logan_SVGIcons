7596f50a1b1c4c4452ab70913bb39ebe44f4264c
logahuff688 <l_huffman159588@fanshaweonline.ca>
2023-01-09 22:33:02 -0500

    updated things

--- js/main.js
+++ js/main.js
@@ -3,5 +3,10 @@ document.addEventListener("click", (event) => {
 
   const elementId = element.getAttribute("id");
 
+  event.target.classList.toggle("selected");
+
   console.log(`You clicked on the element with ID: ${elementId}`);
+  console.log(
+    `the class, "${event.target.classList}" has been added to ${elementId}`
+  );
 });
