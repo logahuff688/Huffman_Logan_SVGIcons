d5e21342a19393d1010e80dd2339c4ed01063910
logahuff688 <113370990+logahuff688@users.noreply.github.com>
2023-01-16 17:44:15 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 console.log('Javascript is running!');
 
-let icons = document.querySelector("#icons") // vectorGraphic variable
+let icons = document.querySelector("#icons") // icons variable
 
 function logID() {
     console.log(this.id);
 }
 
+function logALT() {
+console.log(this.alt); 
+   
+}
+
 icons.addEventListener('click',logID)
-icons.addEventListener('click',logID)
+icons.addEventListener('click',logALT
